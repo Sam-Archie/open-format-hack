37547391c21b36cdcae1998b947aa35e2d9529a4
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-08-17 21:34:50 +0100

    happy now

--- components/header.js
+++ components/header.js
@@ -14,7 +14,7 @@ export default function Header() {
                 alt=""
               />
             </a>
-            <div className="hidden ml-10 space-x-8 lg:block">
+            <div className="ml-10 space-x-8">
               {navigation.map((link) => (
                 <a
                   key={link.name}
@@ -26,17 +26,6 @@ export default function Header() {
               ))}
             </div>
           </div>
-        </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-base font-medium text-white hover:text-indigo-50"
-            >
-              {link.name}
-            </a>
-          ))}
         </div>
       </nav>
     </header>
