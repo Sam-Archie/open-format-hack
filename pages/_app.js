18d0fd914a90017a6b1c6a6c9f5f6d38233616ee
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-08-17 16:42:56 +0100

    init config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { OpenFormatProvider } from "@simpleweb/open-format-react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <OpenFormatProvider config={{ network: "mumbai" }}>
+      <Component {...pageProps} />
+    </OpenFormatProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
